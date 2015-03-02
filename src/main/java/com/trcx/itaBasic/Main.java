49a3c7554c3eb69b6c67bb0cc54f6452113db977
Trcx <trcx528@gmail.com>
2015-03-02 16:32:11 -0600

    fixed the comment field crashing on first startup

--- src/main/java/com/trcx/itaBasic/Main.java
+++ src/main/java/com/trcx/itaBasic/Main.java
@@ -66,11 +66,11 @@ public class Main
         } else {
 //          RegisterArmorMaterial(double protection,Integer enchantability,double speedModifier,Integer maxDurability,String hexColor,String oreDictName){
             RegisterArmorMaterial(2.50,  9,    0, 30, "#D8D8D8", "ingotIron");
-            ITABasic.Materials.get("ingotIron").comment = "Vanilla values";
+            tempMaterials.get(tempMaterials.size() -1).comment = "Vanilla values";
             RegisterArmorMaterial(1.83, 25, -0.2, 14, "#EAEE57", "ingotGold");
-            ITABasic.Materials.get("ingotGold").comment = "Vanilla values (except for speed)";
+            tempMaterials.get(tempMaterials.size() -1).comment = "Vanilla values (except for speed)";
             RegisterArmorMaterial(3.33, 10,    0, 66, "#8CF4E2", "gemDiamond");
-            ITABasic.Materials.get("gemDiamond").comment = "Vanilla values";
+            tempMaterials.get(tempMaterials.size() -1).comment = "Vanilla values";
 
             RegisterArmorMaterial(1.50, 30,    0, 10, "#E0D495", "ingotNickel");
             RegisterArmorMaterial(2.00, 23, -0.1, 16, "#A5BCC3", "ingotSilver");
@@ -90,7 +90,7 @@ public class Main
             RegisterArmorMaterial(3.00, 10, -0.1, 35, "#E39BD3", "ingotAlumite");
             RegisterArmorMaterial(2.40, 12,    0, 25, "#C5C5C5", "ingotAluminum");
             RegisterArmorMaterial(3.50, 35,    0, 45, "#F0F589", "ingotElectrumFlux");
-            ITABasic.Materials.get("ingotElectrumFlux").comment = "Balanced against redstone arsenal";
+            tempMaterials.get(tempMaterials.size() -1).comment = "Balanced against redstone arsenal";
             RegisterArmorMaterial(3.00, 12, 0, 40, "#FF9F1A", "ingotSignalum");
             RegisterArmorMaterial(2.80, 14,    0, 35, "#DDE2E0", "ingotInvar");
             RegisterArmorMaterial(4.50, 20, -0.1, 50, "#107272", "ingotEnderium"); // TODO add teleport?
@@ -103,7 +103,7 @@ public class Main
             RegisterArmorMaterial(1.30,  9, 0.05, 40, "#F0A8A4", "ingotPigIron");
             RegisterArmorMaterial(4.00, 25,    0, 50, "#200D36", "ingotVoid");
             RegisterArmorMaterial(1.00, 25,    0,500,"#737372", "ingotUnstable");
-            ITABasic.Materials.get("ingotUnstable").comment = "the irony, unstable ingots form the most stable(durable) armor";
+            tempMaterials.get(tempMaterials.size() -1).comment = "the irony, unstable ingots form the most stable(durable) armor";
             RegisterArmorMaterial(1.00,  5, -0.3, 21, "#D9DB5C", "ingotYellorium");
             RegisterArmorMaterial(2.00,  7, -0.2, 18, "#4642D6", "ingotBlutonium");
             RegisterArmorMaterial(0.70,  4, -0.4, 19, "#5CAFDB", "ingotCyanite");
