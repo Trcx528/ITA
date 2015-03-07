f87c647f42fc0b6737ed9d82edfa23a9200c62d0
Trcx <trcx528@gmail.com>
2015-03-07 15:32:40 -0600

    updated todo, 0.0.5 jarbuild

--- src/main/java/com/trcx/ita/Main.java
+++ src/main/java/com/trcx/ita/Main.java
@@ -47,7 +47,7 @@ import java.util.*;
 public class Main
 {
 
-    public static final String VERSION = "0.0.4";
+    public static final String VERSION = "0.0.5";
     private static int tickCounter = 0;
 
     public Main(){
