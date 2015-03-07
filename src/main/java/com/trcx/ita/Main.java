9487b294f441f69ecfd7481117ee46791c07c903
Trcx <trcx528@gmail.com>
2015-03-07 00:36:47 -0600

    0.0.4 jarbuild

--- src/main/java/com/trcx/ita/Main.java
+++ src/main/java/com/trcx/ita/Main.java
@@ -52,7 +52,7 @@ import java.util.*;
 public class Main
 {
 
-    public static final String VERSION = "0.0.3";
+    public static final String VERSION = "0.0.4";
     public static Configuration clientConfig;
 
     private static File configDir;
