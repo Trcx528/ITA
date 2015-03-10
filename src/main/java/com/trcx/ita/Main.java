78b2d9527c5d2c5150d60d04d770a852b03e53a7
Trcx <trcx528@gmail.com>
2015-03-10 10:00:45 -0500

    0.0.8 jarbuild

--- src/main/java/com/trcx/ita/Main.java
+++ src/main/java/com/trcx/ita/Main.java
@@ -47,7 +47,7 @@ import java.util.*;
 public class Main
 {
 
-    public static final String VERSION = "0.0.7";
+    public static final String VERSION = "0.0.8";
     private static int tickCounter = 0;
 
     public Main(){
