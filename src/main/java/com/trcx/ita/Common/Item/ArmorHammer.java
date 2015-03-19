c9274580883c8189d583b8464f7e5bf760580744
Trcx <trcx528@gmail.com>
2015-03-19 10:42:53 -0500

    Updated recipe to use ore dictionary and slightly changed recipe (resolves #2)

--- src/main/java/com/trcx/ita/Common/Item/ArmorHammer.java
+++ src/main/java/com/trcx/ita/Common/Item/ArmorHammer.java
@@ -1,11 +1,10 @@
 package com.trcx.ita.Common.Item;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemTool;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 /**
  * Created by Trcx on 2/25/2015.
@@ -15,8 +14,8 @@ public class ArmorHammer extends Item {
     public ArmorHammer() {
         setMaxStackSize(1);
         setContainerItem(this); // player gets to keep this when crafting
-        GameRegistry.addShapedRecipe(new ItemStack(this), " i ", " si", "s  ", 's', new ItemStack(Items.stick),
-                'i', new ItemStack(Items.iron_ingot));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(this), " is", " si", "s  ", 's', new ItemStack(Items.stick),
+                'i', new ItemStack(Items.iron_ingot)));
     }
 
     @Override
